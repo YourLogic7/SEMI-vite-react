628e4aa21245c7c91e6ca1bfe1f1c1439ff63829
YourLogic7 <m.r.izky.nash@gmail.com>
2025-09-21 16:48:24 +0700

    Update product.js

--- backend/models/product.js
+++ backend/models/product.js
@@ -10,7 +10,7 @@ const productSchema = new mongoose.Schema({
   name: { type: String, required: true },
   price: { type: Number, required: true },
   stock: { type: Number, required: true, default: 0 },
-  imageUrl: { type: String, required: true },
+  imageUrl: { type: String },
   images: { type: [String], default: [] },
   sold: { type: Number, default: 0 },
   description: { type: String, required: true },
